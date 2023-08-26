829233fb927d950127f3f2f9c4b1d7bb0d3df235
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2023-08-25 20:51:43 -0400

    updates, we have a working prototype!

--- WebScraperCongress/house.py
+++ WebScraperCongress/house.py
@@ -39,9 +39,20 @@ if __name__ == "__main__":
     if ( len(sys.argv) == 3 ):
         # get politician by last name
         lastName = sys.argv[2]
+        #this is the name of the custom database
         politician_filings = df.loc[df["Last"] == lastName]
-        print(politician_filings)
+        output_text = politician_filings.to_string(index=False, justify='left')
+        print(output_text)
+        politician_filings.to_csv("politician_filings.txt", sep="\t", index=False)
         sys.exit(1)
 
     sort_by_dates = df.sort_values(by=["FilingDate"])
-    print(sort_by_dates)
+    output_text = sort_by_dates.to_string(index=False, justify='right')
+
+    print(output_text)
+
+    # Save the output text to a file
+
+
+
+
