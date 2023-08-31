a958fb5972de3882b3f53fe150c9c368098506a9
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2023-08-31 12:37:40 -0400

    project slider

--- Boxing/script.js
+++ Boxing/script.js
@@ -1,13 +1,15 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import gsap from 'gsap'
 import * as dat from 'dat.gui'
+
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -18,10 +20,15 @@ const scene = new THREE.Scene()
 // 
 let mixer = null
 
+/**
+ * Textures
+ */
+
 // ADD FULL MODEL!!!
 const gltfLoader = new GLTFLoader()
 
-gltfLoader.load('/media/tree/scene.gltf',
+
+gltfLoader.load('/media/tree/low poly survival character.glb',
 
     (gltf) => {
         // const children = [...gltf.scene.children]
@@ -30,6 +37,18 @@ gltfLoader.load('/media/tree/scene.gltf',
         // {
         //     scene.add(child)
         // }
+        const textureLoader = new THREE.TextureLoader()
+        const matcapTexture = textureLoader.load('media/matcaps/8.png')
+
+        // Material
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture }) 
+
+        // Apply material to all meshes in the glTF file
+        gltf.scene.traverse((obj) => {
+            if (obj instanceof THREE.Mesh) {
+                obj.material = material;
+            }
+        });
 
         //Animation
         mixer = new THREE.AnimationMixer(gltf.scene)
@@ -37,7 +56,15 @@ gltfLoader.load('/media/tree/scene.gltf',
 
         action.play()
 
+
+        gltf.scene.scale.set(0.005, 0.005, 0.005)
+        gltf.scene.position.set(-0.3,0,0)
+
+
+
         scene.add(gltf.scene)
+        console.log(gltf.scene)
+  
     }
 )
 
@@ -51,7 +78,7 @@ gltfLoader.load('/media/tree/scene.gltf',
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
 scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
@@ -62,6 +89,9 @@ directionalLight.shadow.camera.bottom = - 7
 directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
+const lightHelperone = new THREE.PointLightHelper(directionalLight)
+//const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelperone)//, gridHelper)
 
 
 /**
@@ -75,7 +105,7 @@ const sizes = {
 window.addEventListener('resize', () =>
 {
     // Update sizes
-    sizes.width = window.innerWidth
+    sizes.width = window.innerWidth / 2
     sizes.height = window.innerHeight
 
     // Update camera
@@ -91,14 +121,24 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 2, 2)
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+camera.position.y = 3
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
+controls.target.set(0.2, 0.9, 0.5)
 controls.enableDamping = true
+controls.enablePan = true
+controls.enableZoom = false
+
+
+
 
 /**
  * Renderer
@@ -108,7 +148,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 
 // clear color background
-// renderer.setClearColor(0x000000, 0);
+renderer.setClearColor(0x000000, 0);
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
@@ -138,11 +178,37 @@ const tick = () =>
     // Update controls
     controls.update()
 
+    // animations
+
     // Render
     renderer.render(scene, camera)
 
+
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+
+
 }
+
+
+
+/**
+ * Debug
+ */
+
+
+window.addEventListener('keydown', (event) =>
+{
+    if(event.key === 'h')
+    {
+        if(gui._hidden)
+            gui.show()
+        else
+            gui.hide()
+    }
+})
+
+
+
 
 tick()
