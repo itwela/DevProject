3ddb17ecf5c330ec2b80c6468ba749f8291d6b51
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2023-09-15 16:38:12 -0400

    completed text detection

--- textdetection/screenshot.py
+++ textdetection/screenshot.py
@@ -3,7 +3,7 @@ import pyautogui as pa
 pa.hotkey("alt", "esc")
 
 # estimate of region for extry and exit controller
-iml = pa.screenshot(region=(1300,130,250,400))
+iml = pa.screenshot(region=(1250,140,300,350))
 iml.save("yo.png")
 
 pa.hotkey("alt", "esc")
--- textdetection/test.py
+++ textdetection/test.py
@@ -1,44 +1,67 @@
-from PIL import Image
 import sys
 from pyautogui import *
 import pyautogui as pa
 import time
 import keyboard
-import random
-import win32api, win32con
+import customtkinter
+import tkinter as tk
 
-time.sleep(2)
 
-flag = False
 
-#  ====================
-    
-# check if buys or sells are opened or closed.
+pa.hotkey("alt", "esc")
+# =======================================================
+
+# =======================================================
+
+# =======================================================
+
+print("Welcome to the SFX Algo. Watch the magic happen now...and to quit for any reason, press ctrl + c in the terminal.")
+
+# =======================================================
+# =======================================================
+
+    # one buy was open
+bo1 = False
+    # one sell is open
+so1 = False 
 while True:
-    if pa.locateOnScreen("openbuy.png", region=(1300,130,250,400), confidence=0.9) != None:
-        if not flag:
-            print("I can see a buy")
-            flag = True
-            time.sleep(0.5)
-
-    # else:
-    #     print("Im legally blind")
-    #     time.sleep(0.5)
-
-    if pa.locateOnScreen("opensell.png", region=(1300,130,250,400), confidence=0.9) != None:
-        if not flag:
-            print("I can see a sell")
-            flag = True
-            time.sleep(0.5)
-
-    # else:
-    #     print("Im legally blind")
-    #     time.sleep(0.5)
-
-    if pa.locateOnScreen("flat.png", region=(1300,130,250,400), confidence=0.9) != None:
-        print("we gone")
-        time.sleep(0.5)
-    
-    if keyboard.is_pressed('q'):
+    if keyboard.is_pressed('shift'):  # if key 'alt' and 'q' are pressed 
+        print("Thank you for using the SFX Algo!")
         sys.exit()
+    if pa.locateOnScreen("openbuy.png", region=(1250,140,300,350), confidence=0.9) != None and bo1 != True:
+        print("I can see a buy")
+        bo1 = True
+        # opens buy
+        pa.hotkey("shift", "b")
+#       # presses enter
+        pa.hotkey("enter")
+    if pa.locateOnScreen("flatbuy.png", region=(1250,140,300,350), confidence=0.96) != None and bo1 != False:
+        print("A buy was closed")
+        bo1 = False
+        # closes buy
+        pa.hotkey("shift", "s")
+#       # presses enter
+        pa.hotkey("enter")
+    if pa.locateOnScreen("opensell.png", region=(1250,140,300,350), confidence=0.9) != None and so1 != True:
+        print("I can see a sell")
+        so1 = True
+        # opens sell
+        pa.hotkey("shift", "s")
+#       # presses enter
+        pa.hotkey("enter")
+    if pa.locateOnScreen("flatsell.png", region=(1250,140,300,350), confidence=0.96) != None and so1 != False:
+        print("A sell was closed")
+        _logs.insert(index="0.0", text="A sell was closed", tags=None)            
+        so1 = False
+        # closes sell
+        pa.hotkey("shift", "b")
+#       # presses enter
+        pa.hotkey("enter")
+    else:
+        pa.moveTo(50,50)
+        time.sleep(60)
+        pa.moveTo(500,50)
+        time.sleep(60)
 
+
+# ========================================================
