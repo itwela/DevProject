d71d0fd1a448d42f6ba4d6f3ad0c12e0c92a94f6
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2023-09-16 17:00:20 -0400

    updated resume

--- textdetection/test.py
+++ textdetection/test.py
@@ -7,27 +7,24 @@ import customtkinter
 import tkinter as tk
 
 
-
+# =======================================================
+# initialization
 pa.hotkey("alt", "esc")
 # =======================================================
 
 # =======================================================
-
-# =======================================================
-
+# Welcome Message
 print("Welcome to the SFX Algo. Watch the magic happen now...and to quit for any reason, press ctrl + c in the terminal.")
-
-# =======================================================
 # =======================================================
 
-    # one buy was open
+# Variables
+# one buy was open
 bo1 = False
-    # one sell is open
+# one sell is open
 so1 = False 
+
+# Loop that watches screen to detect if trades are being opened/closed
 while True:
-    if keyboard.is_pressed('shift'):  # if key 'alt' and 'q' are pressed 
-        print("Thank you for using the SFX Algo!")
-        sys.exit()
     if pa.locateOnScreen("openbuy.png", region=(1250,140,300,350), confidence=0.9) != None and bo1 != True:
         print("I can see a buy")
         bo1 = True
@@ -51,15 +48,13 @@ while True:
         pa.hotkey("enter")
     if pa.locateOnScreen("flatsell.png", region=(1250,140,300,350), confidence=0.96) != None and so1 != False:
         print("A sell was closed")
-        _logs.insert(index="0.0", text="A sell was closed", tags=None)            
         so1 = False
         # closes sell
         pa.hotkey("shift", "b")
 #       # presses enter
         pa.hotkey("enter")
+    # this keeps your screen on. presses alt every minute.
     else:
         pa.hotkey('alt')
         time.sleep(60)
-
-
 # ========================================================
