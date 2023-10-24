cb25d99b3dde2e1e5f12390d4c86ae57864dadf4
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2023-10-23 22:10:30 -0400

    fixed css

--- react-site/tailwind.config.js
+++ react-site/tailwind.config.js
@@ -6,15 +6,10 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'wide': ['wide'], 
-        'main': ['main'], 
+        'main': ['main'],
       },
       colors: {
-        'darkbrown': '#4b3d2d',
         'lightbrown': '#c1b49a',
-      },
-      textShadow: {
-        'custom': '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000'
       },
     },
   },
--- react-site/vite.config.js
+++ react-site/vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-      ],
-  },
-},
+  assetsInclude: '**/*.glb',
 
+  css: {
+},
 })
