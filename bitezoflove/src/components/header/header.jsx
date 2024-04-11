b87f443522a19a015034cb96fffc2ddc23ceb2dd
Itwela Ibomu <36617380+itwela@users.noreply.github.com>
2024-04-11 14:17:03 -0400

    new discount banner added

--- bitezoflove/src/components/header/header.jsx
+++ bitezoflove/src/components/header/header.jsx
@@ -6,6 +6,8 @@ import React, { useState } from 'react';
 import './header.css';  // Remove the duplicate import statement here
 import { IoMdMenu } from "react-icons/io";
 import { NavLink } from 'react-router-dom';
+import Marquee from "react-fast-marquee";
+
 
 const fadeIn = {
   hidden: { opacity: 0 },
@@ -32,36 +34,49 @@ function Header() {
 
   return (
     <>
-      <div className="mobile flex md:hidden">
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          transition={{ duration: 1.6 }}
-          variants={combinedVariantsYtop}
-          className="header-box z-[10000] fixed w-full h-max flex justify-between p-5 place-items-center place-content-center bg-transparent outline-none" 
-        >
-        <img src={logo} className="bg-[#fffbec] p-2 rounded-full logo-img w-[90px] h-[90px] left-0 md:scale-[1.5]" alt=""/>
-        
-        <div onClick={toggleMenu} className={`hamburger rounded-full bg-[#fffbec] p-5  z-[10000]   `}>    
-          <IoMdMenu/>
-        </div>
-        </motion.div>
 
-
-        {/* Menu content */}
-        {menuOpen && (
-          <div className="w-[50vw] right-0 h-[100vh] bg-[#fffbec] fixed z-[1000] menu-content font-second font-[300] text-[1.5em] flex flex-col gap-5 place-items-end pt-[20%] pr-[5%]">
-            {/* Your menu items go here */}
-            <div onClick={toggleMenu}><a href="#">HOME</a></div>
-            <div onClick={toggleMenu}><a href="https://bitez-of-love.myshopify.com/collections/all">SHOP</a></div>
-            <div onClick={toggleMenu}><a href="https://bitez-of-love.myshopify.com/pages/contact">CONTACT</a></div>
-            <a href="#findus"><div onClick={toggleMenu}>FIND US</div></a>
-            <div onClick={toggleMenu}><a href="https://bitez-of-love.myshopify.com/pages/faqs">FAQ</a></div>
-            {/* Add more menu items as needed */}
-          </div>
-        )}
-
+        {/* fast marquee */}
+    <div className="fixed top-0 left-0  w-full h-[43px] z-[1] bg-pink-300">
+      <Marquee className="h-full flex place-items-center ">
+        <p className='w-[100vw] flex gap-1 text-white font-bold'>
+          <span className='text-pink-500'>Bitez of Love Special:</span> Buy 4 Drinks, Get 1 8oz Gel for <span className='underline'>Free</span>
+        </p>
+        <p className='w-[100vw] flex gap-1 text-white font-bold'>
+          <span className='text-pink-500'>Bitez of Love Special:</span> Buy 2 Gels Get 1 16oz Drink for <span className='underline'>Free</span>
+        </p>
+      </Marquee>
+    </div>
+    <div className="flex">
+      
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        transition={{ duration: 1.6 }}
+        variants={combinedVariantsYtop}
+        className="header-box z-[10000] fixed w-full h-max flex justify-between p-5 place-items-center place-content-center bg-transparent top-2 outline-none" 
+      >
+      <img src={logo} className=" bg-[#fffbec] p-2 outline outline-[1px] outline-black rounded-full logo-img w-[90px] h-[90px] left-5 " alt=""/>
+      
+      <div onClick={toggleMenu} className={`md:hidden hamburger outline outline-[1px] outline-black rounded-full bg-[#fffbec] p-5  z-[10000]   `}>    
+        <IoMdMenu/>
       </div>
+      </motion.div>
+
+
+      {/* Menu content */}
+      {menuOpen && (
+        <div className="md:hidden mobile w-[50vw] right-0 h-[100vh] bg-[#fffbec] fixed z-[1000] menu-content font-second font-[300] text-[1.5em] flex flex-col gap-5 place-items-end pt-[20%] pr-[5%]">
+          {/* Your menu items go here */}
+          <div onClick={toggleMenu}><a href="#">HOME</a></div>
+          <div onClick={toggleMenu}><a href="https://bitez-of-love.myshopify.com/collections/all">SHOP</a></div>
+          <div onClick={toggleMenu}><a href="https://bitez-of-love.myshopify.com/pages/contact">CONTACT</a></div>
+          <a href="#findus"><div onClick={toggleMenu}>FIND US</div></a>
+          <div onClick={toggleMenu}><a href="https://bitez-of-love.myshopify.com/pages/faqs">FAQ</a></div>
+          {/* Add more menu items as needed */}
+        </div>
+      )}
+
+    </div>
 
 {/* MOBILE HEADER END ----------------------------- */}
       
@@ -69,7 +84,7 @@ function Header() {
       {/* NOT mobile header - regular header. */}
       <div className="hidden md:flex computer">
         <div
-          className="header-wrapper-big w-max fixed z-[900] md:flex justify-around place-items-center p-5 text-[#000] text-[0.7em] gap-4 font-second font-[900] right-5 top-5 hidden"
+          className="header-wrapper-big w-max fixed z-[900] md:flex justify-around place-items-center p-5 text-[#000] text-[0.7em] gap-4 font-second font-[900] right-5 top-8 hidden"
         >
           <a id="a-tag" className="" href="#">
             HOME
